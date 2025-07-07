db0f149bf760dc1f912d8b63604b9f3591f18bd0
lventuro <lventuropmsjpenal@gmail.com>
2025-07-07 09:37:44 -0500

    mejorando el mensake en consola

---
+++ src/main/java/com/luis/pdfreader/mensaje/Mensaje.java
@@ -0,0 +1,8 @@
+package com.luis.pdfreader.mensaje;
+
+public class Mensaje {
+    public static void main(String[] args) {
+        System.out.println("¡Hola GitHub! Ahora con nueva rama aula01");
+
+    }
+}
