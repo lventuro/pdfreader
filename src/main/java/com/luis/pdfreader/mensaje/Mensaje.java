95808b87d0d20681d0d722fd041dd116566fc84a
lventuro <lventuropmsjpenal@gmail.com>
2025-07-07 10:06:32 -0500

    Agregada ántalla de login

--- src/main/java/com/luis/pdfreader/mensaje/Mensaje.java
+++ src/main/java/com/luis/pdfreader/mensaje/Mensaje.java
@@ -3,6 +3,6 @@ package com.luis.pdfreader.mensaje;
 public class Mensaje {
     public static void main(String[] args) {
         System.out.println("¡Hola GitHub! Ahora con nueva rama aula01");
-
+        System.out.println("Acabamos de crear git checkout -b feature/login");
     }
 }
